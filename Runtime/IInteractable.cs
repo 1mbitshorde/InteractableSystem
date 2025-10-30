34c5a4d9d6acce55d795cf463fe06611f146c8cb
Hyago Oliveira <hyago.oliveira@1mbitshorde.com>
2025-10-30 15:56:33 -0300

    Add IInteractable interface

---
+++ Runtime/IInteractable.cs
@@ -0,0 +1,39 @@
+namespace OneM.InteractableSystem
+{
+    /// <summary>
+    /// Interface used on objects able to be interacted with.
+    /// </summary>
+    public interface IInteractable
+    {
+        /// <summary>
+        /// Whether can interact with this object.
+        /// </summary>
+        /// <returns>True if can interact with this object. False otherwise.</returns>
+        bool CanInteract();
+
+        /// <summary>
+        /// Tries to interact with this object if possible. Shows an interaction fail if not.
+        /// </summary>
+        void TryInteract()
+        {
+            if (CanInteract()) Interact();
+            else ShowInteractionFail();
+        }
+
+        /// <summary>
+        /// Interacts with this object.
+        /// </summary>
+        void Interact();
+
+        /// <summary>
+        /// Shows an interaction fail message.
+        /// </summary>
+        void ShowInteractionFail();
+
+        /// <summary>
+        /// Set this object availability to be interacted.
+        /// </summary>
+        /// <param name="isAvailable">Whether is available.</param>
+        void SetAvailability(bool isAvailable);
+    }
+}
